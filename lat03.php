630c6476b05b3fdfb08ec280f21e899614d0e49b
JefriEkaRakhmansyah <jefrirachmansyah@gmail.com>
2024-02-22 09:11:04 +0700

    video 4

---
+++ lat03.php
@@ -0,0 +1,28 @@
+<?php
+
+  function belajar() {
+    echo"saya belajar php";
+  }
+
+
+  function luaspersegi($p =5, $1 =){
+
+    $luas = $p*$1;
+    echo$luas;
+
+  }
+
+  function output() {
+    return "Belajar Function"
+  }
+
+  echo luas(100,3) * 5;
+
+
+
+output();
+
+
+
+
+?>
