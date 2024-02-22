186c6274e6e58ca806fda901e6e3f739b33d4736
JefriEkaRakhmansyah <jefrirachmansyah@gmail.com>
2024-02-22 09:07:47 +0700

    vidio2

---
+++ lat01.php
@@ -0,0 +1,13 @@
+<?php 
+
+  echo "belajar php";
+  echo '<br>';
+  echo'saya siswa smk';
+  echo '<h1 style="background-color:red"> Belajar PHP itu mudah</h1>
+        <p style="backgrund-color:blue"> Saya Suka belajar program</p>';
+
+   echo 'Saya Belajar','PHP','<br>';
+   echo 2019 + 2;
+
+
+?>
