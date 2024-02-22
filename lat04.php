9a0ce271d92a41cbd17ba6774b2b93446b7650e2
JefriEkaRakhmansyah <jefrirachmansyah@gmail.com>
2024-02-22 09:12:22 +0700

    video 5

---
+++ lat04.php
@@ -0,0 +1,53 @@
+<?php 
+    //OPERATOR MATEMATIKA
+
+    $a =9;
+    $b= 6;
+
+    $c = $a +$b;
+    echo $c.'<br>';
+    $c =$a - $b;
+    echo $c . '<br>';
+
+    $c = $a * $b;
+    echo $c. '<br>';
+
+    $c = $a / $b;
+    echo round ($c) .'<br>';
+
+    $c =$a % $b;
+
+    echo $c. '<br>';
+
+     //OPERATOR LOGIKA
+
+    $c = $a < $b;
+    echo $c.'<br>';
+
+    $c =$a > $b;
+    echo $c . '<br>';
+
+    $c = $a == $b;
+    echo $c. '<br>';
+
+    $c =$a != $b;
+    echo $c. '<br>';
+
+
+    //INCREMENT
+
+    $a++;
+    echo $a. '<br>';
+
+    //OPERATOR STRING
+
+    $kata = 'Sura';
+    $kota = 'baya';
+
+    $hasil = $kata.$kota;
+    $hasil.=' kota pahlawan';
+
+
+    echo $hasil;
+
+?>
